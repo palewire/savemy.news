85e475e10613a0039c4a65f9be67f14c85fe1ab2
palewire <ben.welsh@gmail.com>
2017-11-04 13:08:55 -0700

    This too

--- project/settings.py
+++ project/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '.static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGGING = {
