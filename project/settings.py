145d35903710049b30f4c065343fb4b67d2c1daa
palewire <ben.welsh@gmail.com>
2017-11-05 14:09:18 -0800

    Share buttons

--- project/settings.py
+++ project/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3n1xxp)i%8xrf94p^p83ga0b!nq$h)!#-weajzyk60-bir&uqy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'stark-waters-90707.herokuapp.com',
     'savemy.news',
-    'www.savemy.news'
+    'www.savemy.news',
+    'localhost'
 ]
 
 
