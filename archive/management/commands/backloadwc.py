11c9d29ed803d4511e34979b398f1a0dffde1f31
palewire <ben.welsh@gmail.com>
2017-11-15 21:40:01 -0800

    Fix

--- archive/management/commands/backloadwc.py
+++ archive/management/commands/backloadwc.py
@@ -9,4 +9,4 @@ class Command(BaseCommand):
         clip_list = Clip.objects.all()
         no_wc = [c for c in clip_list if not c.mementos.filter(archive="webcitation.org").count()]
         for c in no_wc[:10]:
-            tasks.wc_memento.delay(clip.id)
+            tasks.wc_memento.delay(c.id)
