af8390a3e4ff3001e8ff41fcfe7968de29dfe555
palewire <ben.welsh@gmail.com>
2019-01-03 05:21:16 -0800

    Slow em down

--- archive/management/commands/backload_wc.py
+++ archive/management/commands/backload_wc.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+import time
 import logging
 from archive import tasks
 from archive.models import Clip
@@ -9,10 +10,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         clip_list = Clip.objects.all().order_by("?")
-
-        # Backload webcitation
         no_wc = [c for c in clip_list if not c.mementos.filter(archive="webcitation.org").count()]
         logger.debug("{} clips lack a webcitation memento".format(len(no_wc)))
         for c in no_wc[:5]:
             logger.debug("Backloading webcitation URL for {}".format(c.url))
             tasks.wc_memento.delay(c.id)
+            time.sleep(5)
