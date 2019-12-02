4c2c9b3eba58b5bc7c8cd05c6e5a6e1e3cd21f09
palewire <ben.welsh@gmail.com>
2019-12-01 19:49:10 -0800

    Upgraded to whitenoise v4.0 configuration guidelines

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
